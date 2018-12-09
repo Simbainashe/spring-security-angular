59161a16ec4805600fc10f2b5cb61eff8e6d6b0b
Fact Simbainashe Musingarimi <fmusingarimi@gmail.com>
2018-12-09 16:33:42 +0200

    Make the application an Oauth2 resource as well

--- auth-server/src/main/java/com/fsm/authserver/ResourceServerConfiguration.java
+++ auth-server/src/main/java/com/fsm/authserver/ResourceServerConfiguration.java
@@ -1,11 +1,14 @@
 package com.fsm.authserver;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
 /**
  * @author Fact S Musingarimi
  * 12/5/18
  * 10:47 PM
  */
-//@Configuration
-//@EnableResourceServer
+@Configuration
+@EnableResourceServer
 public class ResourceServerConfiguration {
 }
